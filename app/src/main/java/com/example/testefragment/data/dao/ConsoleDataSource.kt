de06742fb70dd5b2ca4d18b3b64b5f6d82442c2a
lorranasantos <lorrana.silva11020@gmail.com>
2021-02-18 16:59:48 -0300

    Atualização do layout

--- app/src/main/java/com/example/testefragment/data/dao/ConsoleDataSource.kt
+++ app/src/main/java/com/example/testefragment/data/dao/ConsoleDataSource.kt
@@ -10,8 +10,11 @@ class ConsoleDataSource {
             var consoles = ArrayList<Console>()
 
             consoles.add(Console(1,  Context.getDrawable(R.drawable.playstationone)!!, "PlayStation 1", " Sony", "28-11-1996"))
-            consoles.add(Console(2,  Context.getDrawable(R.drawable.playsatationdois)!!, "PlayStation 2", " Sony", "28-11-2004"))
-            consoles.add(Console(3,  Context.getDrawable(R.drawable.nintendo64)!!, "Nintendo 64", "Nintendo", "28-11-2000"))
+            consoles.add(Console(2,  Context.getDrawable(R.drawable.playsatationdois)!!, "PlayStation 2", " Sony", "04-03-2000"))
+            consoles.add(Console(3,  Context.getDrawable(R.drawable.xboxseriess)!!, "Xbox Series S", "Microsoft", "08-09-2020"))
+            consoles.add(Console(4,  Context.getDrawable(R.drawable.segadreamcast)!!, "Dreamcast", "Sega", "27-11-1998"))
+            consoles.add(Console(5,  Context.getDrawable(R.drawable.playstationfive)!!, "PlayStation 5", "Sony", "28-11-2020"))
+            consoles.add(Console(6,  Context.getDrawable(R.drawable.nintendo64)!!, "Nintendo 64", "Nintendo", "19-11-2000"))
 
             return consoles
         }
